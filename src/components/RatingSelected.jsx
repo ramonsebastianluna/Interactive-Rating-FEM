817159d6cd3da652693659f089158d8bd7441f45
Seba Luna <ramon.sebastian.luna@gmail.com>
2023-09-19 13:17:15 -0300

    feat(component): created RatingSelected component

---
+++ src/components/RatingSelected.jsx
@@ -0,0 +1,13 @@
+const RatingSelected = ({selectedOption}) => {
+    return (
+        <>
+            <p>You selected {selectedOption} out of 5</p>
+            <h2>Thank you!</h2>
+            <p>We appreciated you taking the time to give a rating. If you ever need more support,
+               don´t hesitate to get in touch!
+            </p>
+        </>
+    )
+}
+
+export default RatingSelected;
