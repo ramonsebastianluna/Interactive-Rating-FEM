6859bd3e8be7b55140d323a82ff024e3d161343a
Seba Luna <ramon.sebastian.luna@gmail.com>
2023-09-27 15:19:49 -0300

    feat(sass-styles): finished page one styles

--- src/App.jsx
+++ src/App.jsx
@@ -29,8 +29,8 @@ function App() {
                 <div className="star-container">
                     <img src={star} alt="Star icon" />
                 </div>
-                <h1>How did we do?</h1>
-                <p>Please let us know how we did with your support request. All
+                <h1 className="title-form">How did we do?</h1>
+                <p className="paragraph-form">Please let us know how we did with your support request. All
                    feedback is appreciated to help us improve our offering!
                 </p>
                 <ListRating getSelected={getSelected}/>
