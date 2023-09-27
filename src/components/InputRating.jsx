3240ee676937936f0fc8df068275a02aad477f97
Seba Luna <ramon.sebastian.luna@gmail.com>
2023-09-26 22:23:22 -0300

    fix(component): fixed input status component

--- src/components/InputRating.jsx
+++ src/components/InputRating.jsx
@@ -1,14 +1,21 @@
 
-const InputRating = ({numberOption, idOption, handleIdParent}) => {
+const InputRating = ({numberOption, idOption, handleIdParent, onClick}) => {
 
     const handleId = () => {
         handleIdParent(idOption)
+        onClick(idOption)
     }
 
     return (
         <>
             <label className="rating-number" htmlFor={idOption}>{numberOption}</label>
-            <input className="rating-input" type="radio" id={idOption} name="input" onClick={handleId}/>
+            <input 
+                className="rating-input" 
+                type="radio" 
+                id={idOption} 
+                name="input" 
+                onClick={handleId}
+            />
         </>
     )
 }
--- src/components/ListRating.jsx
+++ src/components/ListRating.jsx
@@ -3,6 +3,11 @@ import InputRating from "./InputRating";
 
 const ListRating = ({getSelected}) => {
     const [isChecked, setIsChecked] = useState(0)
+    const [inputOneIsActive, setInputOneIsActive] = useState(false)
+    const [inputTwoIsActive, setInputTwoIsActive] = useState(false)
+    const [inputThreeIsActive, setInputThreeIsActive] = useState(false)
+    const [inputFourIsActive, setInputFourIsActive] = useState(false)
+    const [inputFiveIsActive, setInputFiveIsActive] = useState(false)
 
     useEffect(()=>{
         getSelected(isChecked)
@@ -11,14 +16,48 @@ const ListRating = ({getSelected}) => {
     const handleIdParent = (idChildren) => {
         setIsChecked(idChildren)
     }
+
+    const setStateActive = (inputActive) => {
+        if (inputActive === 1) {
+            setInputOneIsActive(true);
+            setInputTwoIsActive(false);
+            setInputThreeIsActive(false);
+            setInputFourIsActive(false);
+            setInputFiveIsActive(false);
+        } else if (inputActive === 2) { 
+            setInputOneIsActive(false);
+            setInputTwoIsActive(true);
+            setInputThreeIsActive(false);
+            setInputFourIsActive(false);
+            setInputFiveIsActive(false);
+        } else if (inputActive === 3) {
+            setInputOneIsActive(false);
+            setInputTwoIsActive(false);
+            setInputThreeIsActive(true);
+            setInputFourIsActive(false);
+            setInputFiveIsActive(false);
+        } else if (inputActive === 4) {
+            setInputOneIsActive(false);
+            setInputTwoIsActive(false);
+            setInputThreeIsActive(false);
+            setInputFourIsActive(true);
+            setInputFiveIsActive(false);
+        } else if (inputActive === 5) {
+            setInputOneIsActive(false);
+            setInputTwoIsActive(false);
+            setInputThreeIsActive(false);
+            setInputFourIsActive(false);
+            setInputFiveIsActive(true);
+        }
+    }
     
     return (
         <div className="container-inputs">  
-            <InputRating numberOption={1} idOption={1} handleIdParent={handleIdParent}/>
-            <InputRating numberOption={2} idOption={2} handleIdParent={handleIdParent}/>
-            <InputRating numberOption={3} idOption={3} handleIdParent={handleIdParent}/>
-            <InputRating numberOption={4} idOption={4} handleIdParent={handleIdParent}/>
-            <InputRating numberOption={5} idOption={5} handleIdParent={handleIdParent}/>
+            <InputRating numberOption={1} idOption={1} handleIdParent={handleIdParent} onClick={setStateActive}/>
+            <InputRating numberOption={2} idOption={2} handleIdParent={handleIdParent} onClick={setStateActive}/>
+            <InputRating numberOption={3} idOption={3} handleIdParent={handleIdParent} onClick={setStateActive}/>
+            <InputRating numberOption={4} idOption={4} handleIdParent={handleIdParent} onClick={setStateActive}/>
+            <InputRating numberOption={5} idOption={5} handleIdParent={handleIdParent} onClick={setStateActive}/>
         </div>
     )
 }
