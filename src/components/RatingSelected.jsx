ed8b6eda81d8fb2a18973f411fc81b373a9b9cb4
Seba Luna <ramon.sebastian.luna@gmail.com>
2023-09-27 15:28:38 -0300

    feat(sass-styles): finished page two styles

--- src/components/RatingSelected.jsx
+++ src/components/RatingSelected.jsx
@@ -6,8 +6,8 @@ const RatingSelected = ({selectedOption}) => {
             <img src={thankYou} alt="thank you" />
             <p className='number-selected'>You selected {selectedOption} out of 5</p>
             <h2>Thank you!</h2>
-            <p>We appreciated you taking the time to give a rating. If you ever need more support,
-               don't hesitate to get in touch!
+            <p>We appreciate you taking the time to give a rating. <br/> If you ever need more support,
+               don't hesitate to <br/> get in touch!
             </p>
         </>
     )
